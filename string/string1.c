efcc673eaa1c7a50ea0899ef8c62429feee42e00
Eunice George <georgeunice408@gmail.com>
2023-08-11 16:22:52 +0000

    strings

--- string/string1.c
+++ string/string1.c
@@ -2,6 +2,21 @@
 #include <stdio.h>
 int main ()
 {
-	printf("my name is cain")
+	size_t read;
+	char *string;
+	size_t size;
+	printf("please enter a string\n");
 
+	read = getline(&string, &size, stdin);
+	if(read == -1)
+	{
+		puts("error");
+	} 
+	else{
+		puts("you entered the string:");
+		puts(string);
+		printf("the length of string is %ld", strlen(string));
+	
+	}
+	return 0;
 }
